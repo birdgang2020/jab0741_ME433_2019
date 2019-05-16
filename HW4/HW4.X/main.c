38286bfcd1aa23eba2f303f17a5caab8270275d2
birdgang2020 <49172048+birdgang2020@users.noreply.github.com>
2019-05-16 00:39:33 -0500

    setting up SPI between PIC and DAC, making a header file for all of the initializations for the PIC

---
+++ HW4/HW4.X/main.c
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.c
+ * Author: jbera
+ *
+ * Created on May 16, 2019, 12:26 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    RPB14Rbits.RPB14R = 0b; //Set B14 to SCK1
+    RPB11Rbits.RPB11R = 0b0011; //Set B11 to SDO1
+    SS1bits.SS1R=0b0001l;//B3 to SS1
+    SDI1bits.SDI1R = 0b0010; //Set A4 to SDI4
+    
+            
+            
+    return (EXIT_SUCCESS);
+}
+
