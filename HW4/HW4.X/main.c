386ff8cb9c536fe9dc3706fed1f516f583c0b735
birdgang2020 <49172048+birdgang2020@users.noreply.github.com>
2019-05-16 00:46:47 -0500

    Update main.c

--- HW4/HW4.X/main.c
+++ HW4/HW4.X/main.c
@@ -12,11 +12,20 @@
 /*
  * 
  */
-int main(int argc, char** argv) {
-    RPB14Rbits.RPB14R = 0b; //Set B14 to SCK1
+
+void setVoltage(char channel, int voltage){
+    
+}
+void initSPI1(){
+        RPB14Rbits.RPB14R = 0b; //Set B14 to SCK1
     RPB11Rbits.RPB11R = 0b0011; //Set B11 to SDO1
     RPB3bits.RPB3R=0b001l;//B3 to SS1
     SDI1bits.SDI1R = 0b0010; //Set A4 to SDI4
+}
+
+
+int main(int argc, char** argv) {
+
     
             
             
