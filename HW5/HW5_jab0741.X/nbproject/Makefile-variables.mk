32fbc103abf9c7dd3a3c1ec380e83e3334938e18
birdgang2020 <49172048+birdgang2020@users.noreply.github.com>
2019-05-28 14:21:02 -0500

    Adding starter code to hw5

---
+++ HW5/HW5_jab0741.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=HW5_jab0741.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/HW5_jab0741.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=hw5jab0741.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/hw5jab0741.x.tar
