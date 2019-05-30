748f780f41a1eadbc3a841e1ffb07c6b2cf91c5a
birdgang2020 <49172048+birdgang2020@users.noreply.github.com>
2019-05-30 00:02:14 -0500

    FInalizing code for HW5

--- HW5/HW5_jab0741.X/main.c
+++ HW5/HW5_jab0741.X/main.c
@@ -9,30 +9,34 @@
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
 #include <math.h>
+#include "i2c_master_noint.h"
 
 void initExpander(){
     ANSELBbits.ANSB2 = 0;//Turn off analog for B2
     ANSELBbits.ANSB3 = 0;//Turn off analog for B3
     i2c_master_setup();
 }
+
 void setExpander(char pin,char level){
     i2c_master_start();
     i2c_master_send(0b0100000<<1|0);
     i2c_master_send(pin);
     i2c_master_send(level);
-    i2c_master_stop;
+    i2c_master_stop();
 }
-char getExpander(){
+
+unsigned char getExpander(){
     i2c_master_start();
     i2c_master_send(0b0100000<<1|0);
     i2c_master_send(0x9);
     i2c_master_restart();
     i2c_master_send(0b0100000<<1|1);
-    char r = i2c_master_recv();
+    unsigned char r = i2c_master_recv();
     i2c_master_ack(1);
-    i2c_master_stop;
+    i2c_master_stop();
     return r;
 }
+
 int main() {
     
     __builtin_disable_interrupts();
@@ -49,33 +53,41 @@ int main() {
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
 
-    // do your TRIS and LAT commands here
-    //Set TRIS register to declare I/O, 0 is output, 1 is input.
-    TRISAbits.TRISA4 = 0;   //Declare RA4 (LED) as output
-    TRISBbits.TRISB4 = 1;   //Declare RB4 (Push Button)
-    //Note: Pins should already default to input, so above line may be unneccesary 
+    TRISBbits.TRISB15=0;
+    LATBbits.LATB15=1;
     
     initExpander();
     
+    __builtin_enable_interrupts();
+    
+    //Write to IODIR register to initialize GPIO7 as output
     i2c_master_start();
     i2c_master_send(0b0100000 << 1 | 0);
     i2c_master_send(0x00);
-    i2c_master_send(0xF0);
+    i2c_master_send(0x80);
     i2c_master_stop();
     
+    //Write to OLAT register to initialize GPIO0 as input
     i2c_master_start();
     i2c_master_send(0b0100000 << 1 | 0);
     i2c_master_send(0x0A);
-    i2c_master_send(0x0F);
+    i2c_master_send(0x01);
     i2c_master_stop();
+    
+    _CP0_SET_COUNT(0);
     
     while(1){
         if(getExpander() >>7 ==1){
-        setExpander(0x,1);    
+        setExpander(0xA,1);    
         
     }
         else{
-            setExpander(0x,0)
+            setExpander(0xA,0);
         }
+        if(_CP0_GET_COUNT()>1200000){
+            LATBbits.LATB15=!LATBbits.LATB15;
+            _CP0_SET_COUNT(0);
+        }
+}
 }
 
