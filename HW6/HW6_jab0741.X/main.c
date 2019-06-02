7c06703c88979d4bab7a32c06a01126e8116ecf0
birdgang2020 <49172048+birdgang2020@users.noreply.github.com>
2019-06-02 13:47:42 -0500

    finishing hw6

---
+++ HW6/HW6_jab0741.X/main.c
@@ -0,0 +1,97 @@
+/* 
+ * File:   main.c
+ * Author: jbera
+ *
+ * Created on May 30, 2019, 8:08 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+#include "ili9341.h"
+/*
+ * 
+ */
+void LCD_print_letter(short x,short y,char letter,char color1,char color2 ){
+    letter=letter=0x20;
+    int i=0;
+    for(i;i<5;i++){
+        char col=ASCII[letter,i];
+        for(int j=0;j<8;j++){
+            char bit(col>>j &1);
+            if (bit==1){
+                LCD_drawPixel(x+i,y+i,color1);
+            }
+            else{LCD_drawPixel(x+i,y+i,color2);
+            }
+        }
+    }
+}
+void LCD_print_string(short x,short y,char* str,char color1,char color2){
+    int j=0;
+    while(str[j] && j<26){
+        LCD_print_letter(x+j*5,y,str[j],color1,color2);
+        j++;
+    }
+}
+void LCD_progress_bar(short x,short y,short length,short height,char color){
+    int i=0,j=0;
+    for(i;i<length;i++){
+        for(j;j<height;j++){
+            LCD_drawPixel(x+i,y+j,color);
+        }
+    }
+}
+
+int main() {
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    
+    LCD_init();
+    LCD_clearScreen(0x0000);
+    char message[26];
+    sprintf(message, "Hello World");
+    LCD_print_string(35, 65, message, 0xFFFF, 0x0000);
+    
+    LCD_progress_bar(20, 100, 88, 10, ILI9341_WHITE);
+    LCD_progress_bar(21, 101, 86, 8, ILI9341_BLACK);
+    
+    __builtin_enable_interrupts();
+
+    //Set PIC32 internal clock to 0
+    _CP0_SET_COUNT(0);
+    int count=0;
+    u_int last_tick=0;
+    u_int current_tick=0;
+    while(1){
+        if(count<86){
+            LCD_progress_bar(18+count,100,1,8,ILI9341_RED);
+        }
+        current_tick=_CP0_GET_COUNT();
+        sprintf(message, "fps:%g", 24000000/((float)current_tick - last_tick));
+        LCD_print_string(1,1,message,ILI9341_WHITE,ILI9341_BLACK);
+        last_tick=current_tick;
+            if (_CP0_GET_COUNT() > 2400000){
+            _CP0_SET_COUNT(0);
+            count++;
+            last_tick = 0;
+        }
+    }
+    
+    
+    return (EXIT_SUCCESS);
+}
+
