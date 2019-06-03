01ba39d963ad166b8acb6a1a7fc5d2fb5719844e
birdgang2020 <49172048+birdgang2020@users.noreply.github.com>
2019-06-02 20:56:21 -0500

    trying to debug the lcd screen
    LCD_init() is causing the program to stop because of an infinite loop, can't find errors in the circuitry

--- HW6/HW6_jab0741.X/main.c
+++ HW6/HW6_jab0741.X/main.c
@@ -10,6 +10,7 @@
 #include<xc.h>           // processor SFR definitions
 #include<sys/attribs.h>  // __ISR macro
 #include "ili9341.h"
+#include "initialization.h"
 /*
  * 
  */
@@ -17,20 +18,21 @@ void LCD_print_letter(short x,short y,char letter,char color1,char color2 ){
     letter=letter=0x20;
     int i=0;
     for(i;i<5;i++){
-        char col=ASCII[letter,i];
-        for(int j=0;j<8;j++){
-            char bit(col>>j &1);
-            if (bit==1){
-                LCD_drawPixel(x+i,y+i,color1);
+        char col=ASCII[letter][i];
+        int j=0;
+        for(j;j<8;j++){
+            char current_bit=(col>>j & 1);
+            if (current_bit==1){
+                LCD_drawPixel(x+i,y+j,color1);
             }
-            else{LCD_drawPixel(x+i,y+i,color2);
+            else{LCD_drawPixel(x+i,y+j,color2);
             }
         }
     }
 }
 void LCD_print_string(short x,short y,char* str,char color1,char color2){
     int j=0;
-    while(str[j] && j<26){
+    while((str[j] &&j )<26){
         LCD_print_letter(x+j*5,y,str[j],color1,color2);
         j++;
     }
@@ -58,10 +60,23 @@ int main() {
 
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
-
+    //led tests
+    TRISAbits.TRISA4=0;
+    LATAbits.LATA4=0;
     
+
+
     LCD_init();
+    
+        while(1){
+            if (_CP0_GET_COUNT() >24000000){
+        _CP0_SET_COUNT(0);
+        LATAbits.LATA4= !LATAbits.LATA4;
+        }
+    }
+    
     LCD_clearScreen(0x0000);
+    LCD_drawPixel(100,100,ILI9341_BLACK);
     char message[26];
     sprintf(message, "Hello World");
     LCD_print_string(35, 65, message, 0xFFFF, 0x0000);
@@ -73,10 +88,12 @@ int main() {
 
     //Set PIC32 internal clock to 0
     _CP0_SET_COUNT(0);
+    /*
     int count=0;
     u_int last_tick=0;
     u_int current_tick=0;
     while(1){
+        
         if(count<86){
             LCD_progress_bar(18+count,100,1,8,ILI9341_RED);
         }
@@ -84,12 +101,16 @@ int main() {
         sprintf(message, "fps:%g", 24000000/((float)current_tick - last_tick));
         LCD_print_string(1,1,message,ILI9341_WHITE,ILI9341_BLACK);
         last_tick=current_tick;
-            if (_CP0_GET_COUNT() > 2400000){
+         
+            if (_CP0_GET_COUNT() >24000000){
             _CP0_SET_COUNT(0);
+            LATAbits.LATA4= !LATAbits.LATA4;
             count++;
             last_tick = 0;
         }
     }
+     * */
+     
     
     
     return (EXIT_SUCCESS);
